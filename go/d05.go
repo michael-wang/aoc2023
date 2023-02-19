027bec5578818bdeb13ba2687a926216ca22cfea
Michael Wang <michael.wang@hey.com>
2023-02-20 00:01:15 +0800

    Day5 part 1 done

--- go/d05.go
+++ go/d05.go
@@ -16,8 +16,8 @@ func d05() {
 
 func d05_Part1(data string) (answer string) {
 	count, height := d05_CountStacks(data)
-	ss := newStacks(count)
-	d05_ParseStack(data, ss, height)
+	answer = d05_ParseStack(data, count, height)
+	fmt.Println("[Day05 Par1] ", answer)
 	return
 }
 
@@ -44,38 +44,103 @@ func d05_CountStacks(data string) (count, height int) {
 	panic("Expect row: 1 2 3... but found none")
 }
 
-func d05_ParseStack(data string, ss map[int]*stack.Stack, height int) {
+func d05_ParseStack(data string, numOfStacks, maxStackHeight int) (topCrates string) {
 	f, err := os.Open(data)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to open file: %s", data))
 	}
 	defer f.Close()
 
+	// For simplicity, we parse stack from top to bottom, which creates a
+	// problem: we can't build stack unless we reverse when done parsing.
+	// Fix this by parsign into string, and then build stack with reverse
+	// string order.
+	tt := make([]*string, numOfStacks)
+	for i := 0; i < numOfStacks; i++ {
+		str := ""
+		tt[i] = &str
+	}
+
 	input := bufio.NewScanner(f)
-	for h := 0; h < height; h++ {
+	for h := 0; h < maxStackHeight; h++ {
 		if input.Scan() == false {
 			panic("Premature termination of input data")
 		}
 		row := input.Text()
-		d05_ParseStackRow(row, ss)
+		d05_ParseStackRow(row, tt)
 	}
 
-	for i := 0; i < len(ss); i++ {
-		//stringStackReverse(s)
-		printStack(*ss[i])
+	stacks := make([]*stack.Stack, numOfStacks)
+	for i := 0; i < numOfStacks; i++ {
+		t := tt[i]
+		s := stack.New()
+		fmt.Printf("[%d] ", i)
+		for j := 0; j < len(*t); j++ {
+			str := *t
+			crate := string(str[j])
+			fmt.Printf("%s ", crate)
+			s.Push(string(str[j]))
+		}
+		fmt.Println()
+		stacks[i] = s
+	}
+
+	d05_MoveStacks(input, stacks)
+
+	for _, s := range stacks {
+		if s.Len() > 0 {
+			topCrates += s.Pop().(string)
+		}
 	}
 	return
 }
 
-func d05_ParseStackRow(row string, ss map[int]*stack.Stack) {
+func d05_ParseStackRow(row string, ss []*string) {
 	for i := 1; i < len(row); i += 4 {
 		crate := string(row[i])
 		if crate != " " {
 			is := (i - 1) / 4
 			s := ss[is]
-			s.Push(crate)
-			fmt.Printf("i: %d, crate: %s, #is[%d]: %d\n", i, crate, is, s.Len())
-			//printStack(*s)
+			*s = crate + *s
 		}
+	}
+}
+
+func d05_MoveStacks(input *bufio.Scanner, stacks []*stack.Stack) {
+	// Skip 2 lines to moves.
+	input.Scan()
+	input.Scan()
+
+	for input.Scan() {
+		ss := strings.Split(input.Text(), " from ")
+		s := ss[0][5:]
+		count, err := strconv.Atoi(s)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to parse 'count' from: %s, line: %s", s, input.Text()))
+		}
+
+		ss = strings.Split(ss[1], " to ")
+		s = ss[0]
+		from, err := strconv.Atoi(s)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to parse 'from' from: %s, line: %s", s, input.Text()))
+		}
+
+		s = ss[1]
+		to, err := strconv.Atoi(s)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to parse 'to' from: %s, line: %s", s, input.Text()))
+		}
+
+		fmt.Printf("[%d, %d, %d]\n", count, from, to)
+
+		for i := 0; i < count; i++ {
+			crate := stacks[from-1].Pop()
+			stacks[to-1].Push(crate)
+		}
+	}
+
+	for _, s := range stacks {
+		printStack(*s)
 	}
 }
--- go/d05_test.go
+++ go/d05_test.go
@@ -5,16 +5,18 @@ import (
 	"testing"
 )
 
-func TestD03(t *testing.T) {
+func TestD05(t *testing.T) {
 	expected := "CMZ"
 	got := d05_Part1("../data/d05_example.txt")
 	if got != expected {
 		t.Error(fmt.Sprintf("Part 1 failed: expect %s, but got: %s", expected, got))
 	}
 
-	expected = ""
-	got = d03_Part2("../data/d05_example.txt")
-	if got != expected {
-		t.Error(fmt.Sprintf("Part 2 failed: expect %s, but got: %s", expected, got))
-	}
+	/*
+		expected = ""
+		got = d03_Part2("../data/d05_example.txt")
+		if got != expected {
+			t.Error(fmt.Sprintf("Part 2 failed: expect %s, but got: %s", expected, got))
+		}
+	*/
 }
