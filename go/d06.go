5d84a1a44d8255dcc6cc9cb0796a70e132d3f27b
Michael Wang <michael.wang@hey.com>
2023-02-21 23:13:50 +0800

    Day 6 done

---
+++ go/d06.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func d06() {
+	d06_Part1("../data/d06.txt")
+	d06_Part2("../data/d06.txt")
+}
+
+func d06_Part1(data string) (answer []int) {
+	f, err := os.Open(data)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open file: %s", data))
+	}
+	defer f.Close()
+
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		// Add 1 because quiz ask for index starting from 1.
+		index := d06_FindMarker(input.Text(), 4) + 1
+		answer = append(answer, index)
+	}
+
+	fmt.Printf("[Day 05 Part 1] answer: %d\n", answer)
+	return
+}
+
+func d06_Part2(data string) (answer []int) {
+	f, err := os.Open(data)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open file: %s", data))
+	}
+	defer f.Close()
+
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		// Add 1 because quiz ask for index starting from 1.
+		index := d06_FindMarker(input.Text(), 14) + 1
+		answer = append(answer, index)
+	}
+
+	fmt.Printf("[Day 05 Part 1] answer: %d\n", answer)
+	return
+}
+
+// n is marker length, n = 4 in part 1.
+// Assume len(line) >= n
+func d06_FindMarker(line string, n int) (index int) {
+	rr := []rune(line)
+	for i := (n - 1); i < len(rr); i++ {
+		m := map[rune]int{}
+		for j := i; j > (i - n); j-- {
+			r := rr[j]
+			m[r]++
+			// d06_PrintMap(m)
+			if m[r] > 1 {
+				goto next
+			}
+		}
+		return i
+	next:
+	}
+	panic(fmt.Sprintf("Expect has marker but this line does not: %s", line))
+}
+
+func d06_PrintMap(m map[rune]int) {
+	for k, v := range m {
+		fmt.Printf("%c : %d\n", k, v)
+	}
+}
---
+++ go/d06_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestD06(t *testing.T) {
+	expected := []int{7, 5, 6, 10, 11}
+	got := d06_Part1("../data/d06_example.txt")
+	for i, exp := range expected {
+		if got[i] != exp {
+			t.Error(fmt.Sprintf("Part 1 failed at line: %d, expect answer: %d, but got: %d", i, exp, got[i]))
+		}
+	}
+
+	expected = []int{19, 23, 23, 29, 26}
+	got = d06_Part2("../data/d06_example_p2.txt")
+	for i, exp := range expected {
+		if got[i] != exp {
+			t.Error(fmt.Sprintf("Part 1 failed at line: %d, expect answer: %d, but got: %d", i, exp, got[i]))
+		}
+	}
+}
