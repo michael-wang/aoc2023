00e33c0c9e270f052547752835f2ba83ce57dba4
Michael Wang <michael.wang@hey.com>
2023-03-07 22:50:34 +0800

    Finish day 10 part 2

--- go/d11.go
+++ go/d11.go
@@ -10,11 +10,58 @@ import (
 )
 
 func d11() {
-	d11_Part1("../data/d11_example.txt")
-	// d11_Part2("../data/d11.txt")
+	d11_Part1("../data/d11.txt")
+	d11_Part2("../data/d11.txt")
 }
 
 func d11_Part1(data string) (answer int) {
+	you := d11_You{}
+	you.ParseNote(data)
+
+	for i := 0; i < 20; i++ {
+		you.Round(you.d11_DivideBy3)
+	}
+	you.PrintInspectCounts()
+
+	answer = you.LevelOfMonkeyBusiness()
+	fmt.Println("[Day10 Part 1] answer: ", answer)
+	return
+}
+
+// d11_example.txt
+// M0: new % 23
+// M1: new % 19
+// M2: new % 13
+// M3: new % 17
+func d11_Part2(data string) (answer int) {
+	you := d11_You{}
+	you.ParseNote(data)
+
+	const rounds = 10000
+	for i := 0; i < rounds; i++ {
+		you.Round(you.d11_ModByDivisorProduct)
+	}
+	you.PrintInspectCounts()
+
+	answer = you.LevelOfMonkeyBusiness()
+	fmt.Println("[Day10 Part 2] answer: ", answer)
+	return
+}
+
+type d11_You struct {
+	Monkeys         []*d11_Monkey
+	DivisorProducts int
+}
+
+func (y *d11_You) d11_DivideBy3(old int) (new int) {
+	return old / 3
+}
+
+func (y *d11_You) d11_ModByDivisorProduct(old int) (new int) {
+	return old % y.DivisorProducts
+}
+
+func (y *d11_You) ParseNote(data string) {
 	// Open file
 	f, err := os.Open(data)
 	if err != nil {
@@ -34,71 +81,53 @@ func d11_Part1(data string) (answer int) {
 		// Skip one line
 		input.Scan()
 	}
+	y.Monkeys = monkeys
 
-	round := Round{Monkeys: monkeys}
-	round.Run(20, true)
-	round.PrintInspectCounts()
-	i1, i2 := round.Top2InspectCounts()
-	answer = i1 * i2
-
-	fmt.Println("[Day10 Part 1] answer: ", answer)
-	return
+	y.DivisorProducts = 1
+	for _, m := range monkeys {
+		y.DivisorProducts *= m.TestDivisor
+	}
 }
 
-func d11_Part2(data string) (answer int) {
-	return
-}
-
-type Round struct {
-	Monkeys []*d11_Monkey
-}
-
-func (r *Round) PrintItems() {
+func (r *d11_You) PrintItems() {
 	for i, m := range r.Monkeys {
 		fmt.Printf("Monkey %d: %v\n", i, m.Items)
 	}
 }
 
-func (r *Round) PrintInspectCounts() {
+func (r *d11_You) PrintInspectCounts() {
 	for i, m := range r.Monkeys {
 		fmt.Printf("Monkey %d inspected items %d times.\n", i, m.InspectCount)
 	}
 }
 
-func (r *Round) Top2InspectCounts() (i1, i2 int) {
+func (r *d11_You) LevelOfMonkeyBusiness() (monkeyBusiness int) {
 	ii := []int{}
 	for _, m := range r.Monkeys {
 		ii = append(ii, m.InspectCount)
 	}
-	i1, ii = intSlicePopMax(ii)
-	i2, _ = intSlicePopMax(ii)
-	return
+	i1, ii := intSlicePopMax(ii)
+	i2, _ := intSlicePopMax(ii)
+	return i1 * i2
 }
 
-func (r *Round) Run(round int, worryLevelDown bool) {
+func (r *d11_You) Round(worryLevelDown func(old int) (new int)) {
 	// moduloProduct := 1
 	// for _, m := range r.Monkeys {
 	// 	moduloProduct *= m.TestDivisor
 	// }
-	for i := 0; i < round; i++ {
-		for _, m := range r.Monkeys {
-			for item, more := m.InspectItem(); more; item, more = m.InspectItem() {
-				// Worry level down
-				if worryLevelDown {
-					item = item / 3
-					// } else {
-					// item = item % moduloProduct
-				}
+	for _, m := range r.Monkeys {
+		for item, more := m.InspectItem(); more; item, more = m.InspectItem() {
+			item = worryLevelDown(item)
 
-				// Monkey test item
-				divisible := item%m.TestDivisor == 0
-				if divisible {
-					// True
-					r.Monkeys[m.TrueMonkey].AcceptItem(item)
-				} else {
-					// False
-					r.Monkeys[m.FalseMonkey].AcceptItem(item)
-				}
+			// Monkey test item
+			divisible := item%m.TestDivisor == 0
+			if divisible {
+				// True
+				r.Monkeys[m.TrueMonkey].AcceptItem(item)
+			} else {
+				// False
+				r.Monkeys[m.FalseMonkey].AcceptItem(item)
 			}
 		}
 	}
--- go/d11_test.go
+++ go/d11_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"sort"
 	"testing"
 )
 
@@ -11,4 +13,109 @@ func TestD11_Part1(t *testing.T) {
 		t.Errorf("Day 11 Part 1 failed, expect answer: %d, but got: %d", expected, got)
 	}
 
+}
+
+func TestD11_Part2(t *testing.T) {
+	expected := map[int]map[int]int{
+		1: {
+			0: 2,
+			1: 4,
+			2: 3,
+			3: 6,
+		},
+		20: {
+			0: 99,
+			1: 97,
+			2: 8,
+			3: 103,
+		},
+		1000: {
+			0: 5204,
+			1: 4792,
+			2: 199,
+			3: 5192,
+		},
+		2000: {
+			0: 10419,
+			1: 9577,
+			2: 392,
+			3: 10391,
+		},
+		3000: {
+			0: 15638,
+			1: 14358,
+			2: 587,
+			3: 15593,
+		},
+		4000: {
+			0: 20858,
+			1: 19138,
+			2: 780,
+			3: 20797,
+		},
+		5000: {
+			0: 26075,
+			1: 23921,
+			2: 974,
+			3: 26000,
+		},
+		6000: {
+			0: 31294,
+			1: 28702,
+			2: 1165,
+			3: 31204,
+		},
+		7000: {
+			0: 36508,
+			1: 33488,
+			2: 1360,
+			3: 36400,
+		},
+		8000: {
+			0: 41728,
+			1: 38268,
+			2: 1553,
+			3: 41606,
+		},
+		9000: {
+			0: 46945,
+			1: 43051,
+			2: 1746,
+			3: 46807,
+		},
+		10000: {
+			0: 52166,
+			1: 47830,
+			2: 1938,
+			3: 52013,
+		},
+	}
+
+	keys := []int{}
+	for k, _ := range expected {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	for _, round := range keys {
+		you := d11_You{}
+		you.ParseNote("../data/d11_example.txt")
+
+		for i := 0; i < round; i++ {
+			you.Round(you.d11_ModByDivisorProduct)
+		}
+
+		fmt.Println("== After round", round, "==")
+		for monkey, exp := range expected[round] {
+			got := you.Monkeys[monkey].InspectCount
+			if got == exp {
+				fmt.Printf("Monkey %d inspected item %d times\n", monkey, got)
+			} else {
+				fmt.Printf("Monkey %d inspected item %d times (expected: %d)\n", monkey, got, exp)
+				t.Errorf("Error, expected count: %d, got count: %d\n", exp, got)
+			}
+		}
+	}
+
+	// for round := 0;
 }
